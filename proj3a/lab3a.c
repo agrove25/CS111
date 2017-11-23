e230e87123738b1e047a7917778489799f74d85d
weimouhe <weimouhe@gmail.com>
2017-11-23 01:24:23 -0800

    I added the inode_summary()

--- proj3a/lab3a.c
+++ proj3a/lab3a.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <errno.h>
-
+#include <time.h>
 #include <fcntl.h>
 
 #include "ext2_fs.h"
@@ -18,57 +18,142 @@ struct ext2_group_desc group;
 int block_size;
 
 void handleError(char loc[256], int err) {
-  fprintf(stderr, "Error encountered in ");
-  fprintf(stderr, "%s", loc);
-  fprintf(stderr, ": ");
-  fprintf(stderr, "%s\n", strerror(err));
-
-  exit(1);
+    fprintf(stderr, "Error encountered in ");
+    fprintf(stderr, "%s", loc);
+    fprintf(stderr, ": ");
+    fprintf(stderr, "%s\n", strerror(err));
+    
+    exit(1);
 }
 
 void processArguments(int argc, char* argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Requires an image file.\n");
-    exit(1);
-  }
-
-  fd_image = open(argv[1], O_RDONLY);
-  if (fd_image < 0) {
-    handleError("processArguments (open)", errno);
-  }
+    if (argc != 2) {
+        fprintf(stderr, "Requires an image file.\n");
+        exit(1);
+    }
+    
+    fd_image = open(argv[1], O_RDONLY);
+    if (fd_image < 0) {
+        handleError("processArguments (open)", errno);
+    }
 }
 
 void analyzeSuper() {
-  pread(fd_image, &super, sizeof(struct ext2_super_block), 1024);
-  block_size = 1024 << super.s_log_block_size;
-
-  fprintf(stdout, "SUPERBLOCK,");
-  fprintf(stdout, "%d,", super.s_blocks_count);
-  fprintf(stdout, "%d,", super.s_inodes_count);
-  fprintf(stdout, "%d,", block_size);  // SPEC
-  fprintf(stdout, "%d,", super.s_inode_size);
-  fprintf(stdout, "%d,", super.s_blocks_per_group);
-  fprintf(stdout, "%d,", super.s_inodes_per_group);
-  fprintf(stdout, "%d\n", super.s_first_ino);
+    pread(fd_image, &super, sizeof(struct ext2_super_block), 1024);
+    block_size = 1024 << super.s_log_block_size;
+    
+    fprintf(stdout, "SUPERBLOCK,");
+    fprintf(stdout, "%d,", super.s_blocks_count);
+    fprintf(stdout, "%d,", super.s_inodes_count);
+    fprintf(stdout, "%d,", block_size);  // SPEC
+    fprintf(stdout, "%d,", super.s_inode_size);
+    fprintf(stdout, "%d,", super.s_blocks_per_group);
+    fprintf(stdout, "%d,", super.s_inodes_per_group);
+    fprintf(stdout, "%d\n", super.s_first_ino);
 }
 
 // WE ONLY HAVE TO DEAL WITH SINGLE GROUP SYSTEMS (PIAZZA)
 void analyzeGroup() {
-  int offset = super.s_first_data_block ? block_size * 2 : block_size;
-  pread(fd_image, &group, sizeof(struct ext2_group_desc), offset);
-
-  fprintf(stdout, "GROUP,0,");  // SINGLE GROUP SYSTEM
-  fprintf(stdout, "%d,", super.s_blocks_count);  // DUE TO SINGLE GRP
-  fprintf(stdout, "%d,", super.s_inodes_per_group);
-  fprintf(stdout, "%d,", group.bg_free_blocks_count);
-  fprintf(stdout, "%d,", group.bg_free_inodes_count);
-  fprintf(stdout, "%d,", group.bg_block_bitmap);
-  fprintf(stdout, "%d,", group.bg_inode_bitmap);
-  fprintf(stdout, "%d\n", group.bg_inode_table);
+    int offset = super.s_first_data_block ? block_size * 2 : block_size;
+    pread(fd_image, &group, sizeof(struct ext2_group_desc), offset);
+    
+    fprintf(stdout, "GROUP,0,");  // SINGLE GROUP SYSTEM
+    fprintf(stdout, "%d,", super.s_blocks_count);  // DUE TO SINGLE GRP
+    fprintf(stdout, "%d,", super.s_inodes_per_group);
+    fprintf(stdout, "%d,", group.bg_free_blocks_count);
+    fprintf(stdout, "%d,", group.bg_free_inodes_count);
+    fprintf(stdout, "%d,", group.bg_block_bitmap);
+    fprintf(stdout, "%d,", group.bg_inode_bitmap);
+    fprintf(stdout, "%d\n", group.bg_inode_table);
 }
 
+/*
+ 
+ INODE
+ inode number (decimal)
+ file type ('f' for file, 'd' for directory, 's' for symbolic link, '?" for anything else)
+ mode (low order 12-bits, octal ... suggested format "0%o")
+ owner (decimal)
+ group (decimal)
+ link count (decimal)
+ time of last I-node change (mm/dd/yy hh:mm:ss, GMT)
+ modification time (mm/dd/yy hh:mm:ss, GMT)
+ time of last access (mm/dd/yy hh:mm:ss, GMT)
+ file size (decimal)
+ number of blocks (decimal)
+ The next fifteen fields are block addresses (decimal, 12 direct, one indirect, one double indirect, one triple indirect).
+ */
+void inode_summary()
+{
+    int inode_table_offset = group.bg_inode_table * block_size;
+    // int inodeTableOffset = SUPEROFF + 4*BLOCKSIZE; // offset at which inodeTable startds
+    
+    
+    struct ext2_inode current_inode;
+    
+    ssize_t i = 0;
+    for (i = 0; i < super.s_inodes_count; i++)   //for every inodes
+    {
+        
+        //store infos to current_inode
+        pread(fd_image, &current_inode, sizeof(struct ext2_inode), inode_table_offset + i * sizeof(struct ext2_inode));
+        
+        //process current_inode
+        if ( (current_inode.i_mode) != 0 && (current_inode.i_links_count) != 0) {
+            __u32 inode_number = i+1;
+            
+            char fileType = '?';
+            if (current_inode.i_mode & 0xA000)
+                fileType = 's';
+             if (current_inode.i_mode & 0x8000)
+                fileType = 'f';
+             if (current_inode.i_mode & 0x4000)
+                fileType = 'd';
+            
+            __u16 mode = current_inode.i_mode & 0xFFF;
+            __u16 owner = current_inode.i_uid;
+            __u16 group = current_inode.i_gid;
+            __u16 link_count = current_inode.i_links_count;
+            
+            struct tm timestamp;
+            
+            char creation_time[19];  //??time of last I-node change
+            time_t c_timestamp = current_inode.i_ctime;
+            timestamp = *gmtime(&c_timestamp);
+            strftime(creation_time, 19, "%m/%d/%y %H:%M:%S", &timestamp);
+            
+            char modification_time[19];
+            time_t m_timestamp = current_inode.i_mtime;
+            timestamp = *gmtime(&m_timestamp);
+            strftime(modification_time, 19, "%m/%d/%y %H:%M:%S", &timestamp);
+            
+            char access_time[19];
+            time_t a_timestamp = current_inode.i_atime;
+            timestamp = *gmtime(&a_timestamp);
+            strftime(access_time, 19, "%m/%d/%y %H:%M:%S", &timestamp);
+            
+            __u32 file_size = current_inode.i_size;
+            __u32 num_blocks = current_inode.i_blocks;
+            
+            fprintf(stdout, "INODE,%u,%c,%o,%u,%u,%u,%s,%s,%s,%u,%u",inode_number,fileType,mode,owner,group,link_count,creation_time,modification_time,access_time,file_size,num_blocks);
+            
+            //print out addresses of blocks
+            int j=0;
+            for (j=0; j < 15 ; j++ )
+            {
+                fprintf(stdout, ",%u",current_inode.i_block[j]);
+            }
+	    fprintf(stdout,"\n");
+        }
+        
+        
+    }
+}
+
+
 int main(int argc, char* argv[]) {
-  processArguments(argc, argv);
-  analyzeSuper();
-  analyzeGroup();
+    processArguments(argc, argv);
+    analyzeSuper();
+    analyzeGroup();
+    inode_summary();
 }
