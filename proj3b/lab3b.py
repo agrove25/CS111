e969b89c3b753544b74ac16e0c6331f8eeee3151
Andrew Koeswanto <koeswantoandrew@gmail.com>
2017-11-28 14:14:33 -0800

    INVALID DIRECT BLOCK

--- proj3b/lab3b.py
+++ proj3b/lab3b.py
@@ -1,12 +1,43 @@
 import csv
 import sys
 
+class Inode:
+    def __init__(self, inode_num, n_links):
+        self.inode_num = inode_num
+        self.n_links = n_links
+        self.state = 'USED'
+
+class Block:
+    def __init__(self, block_num):
+        self.block_num = block_num
+        self.inodes = set()
+        self.state = 'USED'
+
 class Analyzer:
     def __init__(self, input):
         self.csv_file = input
         self.n_blocks = 0
         self.n_inodes = 0
         self.inode_per_group = 0
+
+        self.inodes = dict()
+        self.blocks = dict()
+
+    #---- PRINT FUNCTIONS ----#
+
+    def print_indirect(self, b_num, i_num, offset, indirect):
+        if (indirect == 0):
+            print("INVALID BLOCK {} IN INODE {} AT OFFSET {}\n".format(b_num, i_num, offset))
+        elif (indirect > 0):
+            print("Haven't added this functionality (print_indirect)")
+
+    #---- ANALYSIS FUNCTIONS ----#
+
+    def add_block(self, b_num, i_num, offset, indirect):
+        if b_num < 0 or b_num >= self.n_blocks:
+            self.print_indirect(b_num, i_num, offset, indirect)
+
+
 
     #---- READER FUNCTIONS ----#
 
@@ -15,11 +46,21 @@ class Analyzer:
         self.n_inodes = int(entry[2])
         self.inode_per_group = int(entry[6])
 
-    def read_entry(self, entry):
-        print(entry[0])
+    def read_inode(self, entry):
+        i_num = int(entry[1])
+        self.inodes[entry[1]] = Inode(i_num, int(entry[6]))
+        n_addresses = int(entry[11])
 
+        for i in range(12, 12 + min(n_addresses, 12)):
+            b_num = int(entry[i])
+            self.add_block(b_num, i_num, 8888, 0)
+
+    def read_entry(self, entry):
         if (entry[0] == 'SUPERBLOCK'):
             self.read_superblock(entry)
+        elif (entry[0] == 'INODE'):
+            self.read_inode(entry)
+
 
     def read_csv(self):
         reader = csv.reader(self.csv_file, delimiter=',')
@@ -27,8 +68,6 @@ class Analyzer:
         for r in reader:
             self.read_entry(r)
 
-
-    #----- ANALYSIS FUNCTIONS -----#
 
 
 if __name__ == "__main__":
