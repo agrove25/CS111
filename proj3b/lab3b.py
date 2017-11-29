f6a94e6b701a5c32e446ee961b74dee4cc5df4d3
Andrew Koeswanto <koeswantoandrew@gmail.com>
2017-11-28 19:23:18 -0800

    INITIAL SETUP

--- proj3b/lab3b.py
+++ proj3b/lab3b.py
@@ -1,86 +1,94 @@
 import csv
 import sys
 
-class Inode:
-    def __init__(self, inode_num, n_links):
-        self.inode_num = inode_num
-        self.n_links = n_links
-        self.state = 'USED'
+# ------------ FILE SYSTEM INFORMATION ---------------#
+superblock = None
+group = None
+free_inodes = []
+free_blocks = []
+inodes = {}     # indexes are inode numbers
+dirents = []
+indirects = []
 
-class Block:
-    def __init__(self, block_num):
-        self.block_num = block_num
-        self.state = 'USED'
 
-class Analyzer:
-    def __init__(self, input):
-        self.csv_file = input
-        self.n_blocks = 0
-        self.n_inodes = 0
-        self.inode_per_group = 0
-
-        self.inodes = dict()
-        self.blocks = dict()
-
-    #---- PRINT FUNCTIONS ----#
-
-    def print_invalid(self, b_num, i_num, offset, indirect):
-        print("INVALID ", end="")
-
-        if (indirect == 1):
-            print("INDIRECT ", end="")
-        elif (indirect == 2):
-            print("DOUBLE INDIRECT ", end="")
-        elif (indirect == 3):
-            print("TRIPLE INDIRECT ", end="")
-
-        print("BLOCK {} IN INODE {} AT OFFSET {}".format(b_num, i_num, offset))
-
-    #---- ANALYSIS FUNCTIONS ----#
-
-    def add_block(self, b_num, i_num, offset, indirect):
-        if b_num < 0 or b_num >= self.n_blocks:
-            self.print_invalid(b_num, i_num, offset, indirect)
-        else:
-            self.blocks[b_num] = Block(b_num)
-
-    #---- READER FUNCTIONS ----#
-
-    def read_superblock(self, entry):
+class Superblock:
+    def __init__(self, entry):
         self.n_blocks = int(entry[1])
         self.n_inodes = int(entry[2])
-        self.inode_per_group = int(entry[6])
+        self.block_size = int(entry[3])
+        self.inode_size = int(entry[4])
+        self.blocks_per_group = int(entry[5])
+        self.inodes_per_group = int(entry[6])
+        self.first_nonreserved_inode = int(entry[7])
 
-    def read_inode(self, entry):
-        i_num = int(entry[1])
-        self.inodes[entry[1]] = Inode(i_num, int(entry[6]))
-        n_addresses = int(entry[11])
+class Group:
+    def __init__(self, entry):
+        self.group_num = int(entry[1])
+        self.n_blocks = int(entry[2])
+        self.n_inodes = int(entry[3])
+        self.n_free_blocks = int(entry[4])
+        self.n_free_inodes = int(entry[5])
+        self.block_num_free_block_bitmap = int(entry[6])
+        self.block_num_free_inode_bitmap = int(entry[7])
+        self.block_num_first_inode  = int(entry[8])
 
-        for i in range(12, 24):
-            self.add_block(int(entry[i]), i_num, i-12, 0)
-        self.add_block(int(entry[24]), i_num, 12, 1)
-        self.add_block(int(entry[25]), i_num, 268, 2)
-        self.add_block(int(entry[26]), i_num, 65804, 3)
+class Inode:
+    def __init__(self, entry):
+        self.inode_num = int(entry[1])
+        self.inode_file = entry[2]
+        self.mode = int(entry[3], 8)
+        self.owner = int(entry[4])
+        self.group = int(entry[5])
+        self.link_count = int(entry[6])
+        self.last_change = entry[7]
+        self.last_modified = entry[8]
+        self.last_accessed = entry[9]
+        self.file_size = int(entry[10])
+        self.n_blocks = int(entry[11])
+        self.blocks = []
 
-        # NOT SURE IF THE ABOVE OR THE ONE BELOW
-        """
-        for i in range(12, 12 + min(n_addresses, 12)):
-            b_num = int(entry[i])
-            print(b_num)
-            self.add_block(b_num, i_num, 8888, 0)
-        if (n_addresses >= 13):
-            self.add_block(int(entry[24]), i_num, 8888, 1)
-        if (n_addresses >= 14):
-            self.add_block(int(entry[25]), i_num, 8888, 2)
-        if (n_addresses >= 15):
-            self.add_block(int(entry[26]), i_num, 8888, 3)
-        """
+        for i in range(0, 15):
+            self.blocks.append(int(entry[i + 12]))
+
+class Dirent:
+    def __init__(self, entry):
+        self.parent_inode_num = int(entry[1])
+        self.byte_offset = int(entry[2])
+        self.ref_inode_num = int(entry[3])
+        self.entry_len = int(entry[4])
+        self.name_len = int(entry[5])
+        self.name = entry[6]
+
+class Indirect:
+    def __init__(self, entry):
+        self.own_inode_num = int(entry[1])
+        self.indirect_level = int(entry[2])
+        self.byte_offset = int(entry[3])
+        self.scan_block_num = int(entry[4])
+        self.ref_block_number = int(entry[5])
+
+class Reader:
+    def __init__(self, csv_file):
+        self.csv_file = csv_file
 
     def read_entry(self, entry):
         if (entry[0] == 'SUPERBLOCK'):
-            self.read_superblock(entry)
+            superblock = Superblock(entry)
+        elif (entry[0] == 'GROUP'):
+            group = Group(entry)
+        elif (entry[0] == 'BFREE'):
+            free_blocks.append(int(entry[1]))
+        elif (entry[0] == 'IFREE'):
+            free_inodes.append(int(entry[1]))
         elif (entry[0] == 'INODE'):
-            self.read_inode(entry)
+            inodes[int(entry[1])] = Inode(entry)
+        elif (entry[0] == 'DIRENT'):
+            dirents.append(Dirent(entry))
+        elif (entry[0] == 'INDIRECT'):
+            indirects.append(Indirect(entry))
+        else:
+            print("INVALID CSV FILE")
+            exit(1)
 
 
     def read_csv(self):
@@ -90,10 +98,9 @@ class Analyzer:
             self.read_entry(r)
 
 
-
 if __name__ == "__main__":
     csv_file = open(sys.argv[1], 'r')
-    analyzer = Analyzer(csv_file)
-    analyzer.read_csv()
+    reader = Reader(csv_file)
+    reader.read_csv()
 
     csv_file.close()
